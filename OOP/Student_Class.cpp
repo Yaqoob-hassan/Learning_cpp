d4ffedb09942b72077b37e7059f249120be7f49d
Muhammad Yaqoob Hassan <myaqoobh13@gmail.com>
2026-07-06 19:33:32 +0500

    Add Student Class program

---
+++ OOP/Student_Class.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+class student {
+   private:
+
+    int regno;
+    string name;
+    string department;
+
+    public:
+
+    void Regno() {
+        cout << "Enter your registration number :";
+        cin >> regno;
+    }
+
+    void Name() {
+        cout << "Enter your name :";
+        cin >> name;
+    }
+
+    void Department() {
+        cout << "Enter your department :";
+        cin >> department;
+    }
+
+    void display() {
+        cout << "Registration no is :" << regno << endl;
+        cout << "Registration no is :" << name << endl;
+        cout << "Registration no is :" << department << endl;
+
+    }
+
+};
+
+int main() {
+    student s1;
+
+    s1.Regno();
+    s1.Name();
+    s1.Department();
+
+
+    s1.display();
+    
+
+    return 0;
+}
