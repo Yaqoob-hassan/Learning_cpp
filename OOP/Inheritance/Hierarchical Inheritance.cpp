eae1081c8beb7c2075f51912669fa6096d7f6624
Muhammad Yaqoob Hassan <myaqoobh13@gmail.com>
2026-08-14 14:32:31 +0500

    Add hierarchical inheritance example

--- OOP/Inheritance/Hierarchical Inheritance.cpp
+++ OOP/Inheritance/Hierarchical Inheritance.cpp
@@ -22,7 +22,12 @@ class payment {
     cout << "Amount must be greater than zero." << endl;
     return; }
          cout << " -------------------------------- " << endl;
-        cout << "Processing payment of " << amount  << endl;
+        cout << "Processing payment of: " << amount  << endl;
+    }
+
+    virtual ~payment() {
+        cout << " -------------------------------- " << endl;
+        cout << "Payment destructor." << endl;
     }
 
 };
@@ -40,8 +45,10 @@ class CreditCard : public payment{
 
         cout << " -------------------------------- " << endl;
        cout << "Payment processing with credit card: " << amount << endl;
-    
-
+}
+    ~CreditCard() {
+        cout << " -------------------------------- " << endl;
+    cout << "Credit Card destructor." << endl;
 }
 };
 
@@ -58,21 +65,28 @@ class UpiPayment : public payment{
         cout << "The amount is : " << amount << endl;
        }
 
-
+    ~UpiPayment() {
+        cout << " -------------------------------- " << endl;
+        cout << "Destrucotor of UpiPayment." << endl;
+    }
 };
 
 int main() {
     payment* p = new CreditCard;
     payment* p1 = new UpiPayment;
+    payment* p2 = new payment;
+
+
 
 
     p->processPayment(21);
     p1->processPayment(44);
-    
+    p2->processPayment(444);
 
 
     delete p;
     delete p1;
+    delete p2;
     
     return 0;
 }
