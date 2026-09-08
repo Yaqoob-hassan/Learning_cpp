b9ead1c352dbd5e063aed9ea64e3c3e7c6ee7e2a
Muhammad Yaqoob Hassan <myaqoobh13@gmail.com>
2026-09-08 15:57:54 +0500

    Friend Function

--- OOP/Advance_Concept/Friends_Function.cpp
+++ OOP/Advance_Concept/Friends_Function.cpp
@@ -1,7 +1,46 @@
 #include<iostream>
 using namespace std;
 
-int main() {
+// Friend class is a special class that can access private & Protected member of a class. 
+
+class X {
+  private: 
+    int x; 
+    int y; 
+  
+ public:
     
-    return 0;
+   void show() {
+    cout << "Value of x is :" << x << endl;
+    cout << "Value of y is :" << y << endl;
+   }
+     
+   friend class Y;
+
+
+};
+
+class Y {
+   private: 
+     int z; 
+
+   public: 
+     
+     void set(X &obj) {
+        obj.x = 20;
+        obj.y = 19;
+     }
+
+     
+};
+
+int main() {
+    X x;
+    Y y;
+
+    y.set(x);
+
+    x.show();
+
+    return 0; 
 }
